a32acef3984404a6404d4c72fa21040c551d7ba3
Takfes <takis_a29@hotmail.com>
2020-03-05 22:43:02 +0200

    first version of doc2vec & most similar functionality

--- process_text.py
+++ process_text.py
@@ -100,25 +100,104 @@ len(token.vector)
 len(doc.vector)
 
 # ============================================================================================
-# Custom Text Preprocessing Function
+# Text Preprocessing and model building
 # ============================================================================================
 
-def prep_corpus(series):
-    # helper
-    RE_PUNCTUATION = '|'.join([re.escape(x) for x in string.punctuation])
-    stopwords = set(STOPWORDS)
-    lemmatizer = WordNetLemmatizer()
 
-    # PREPROCESSING
-    body0 = series.str.lower().str.replace(RE_PUNCTUATION, ' ').str.strip().str.split()
-    body1 = body0.apply(lambda tokens: [token for token in tokens if token not in stopwords])
-    body2 = body1.apply(lambda tokens: [lemmatizer.lemmatize(token) for token in tokens])
+import gensim
+from gensim.models.doc2vec import Doc2Vec, TaggedDocument
+from nltk.tokenize import word_tokenize
 
-    # corpus for analysis
-    corpus = [" ".join(x) for x in body2]
-    return corpus
 
-corpus = prep_corpus(df01.text)
+# def prep_corpus(series):
+#     # helper
+#     RE_PUNCTUATION = '|'.join([re.escape(x) for x in string.punctuation])
+#     stopwords = set(STOPWORDS)
+#     lemmatizer = WordNetLemmatizer()
+#
+#     # PREPROCESSING
+#     body0 = series.str.lower().str.replace(RE_PUNCTUATION, ' ').str.strip().str.split()
+#     body1 = body0.apply(lambda tokens: [token for token in tokens if token not in stopwords])
+#     body2 = body1.apply(lambda tokens: [lemmatizer.lemmatize(token) for token in tokens])
+#
+#     # corpus for analysis
+#     corpus = [" ".join(x) for x in body2]
+#     return corpus
+#
+# corpus = prep_corpus(df01.text)
+#
+# train_corpus = [TaggedDocument(words=word_tokenize(_d.lower()), tags=[i]) for i, _d in enumerate(corpus)]
+
+
+def read_corpus(text_iter, tokens_only=False):
+    for i, line in enumerate(text_iter):
+        tokens = gensim.utils.simple_preprocess(line)
+        if tokens_only:
+            yield tokens
+        else:
+            yield gensim.models.doc2vec.TaggedDocument(tokens, [i])
+
+train_corpus = list(read_corpus(df01.text))
+test_corpus = list(read_corpus(df01.text, tokens_only=True))
+
+
+max_epochs = 500
+vec_size = 100
+alpha = 0.025
+window = 20
+epochs = 100
+
+model = Doc2Vec(size=vec_size,
+                alpha=alpha,
+                epochs=epochs,
+                min_count=5,
+                window=window,
+                min_alpha=0.00025,
+                dm=1)
+
+model.build_vocab(train_corpus)
+
+model.train(train_corpus, total_examples=model.corpus_count, epochs=model.epochs)
+
+ranks = []
+second_ranks = []
+for doc_id in range(len(train_corpus)):
+    inferred_vector = model.infer_vector(train_corpus[doc_id].words)
+    sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+    rank = [docid for docid, sim in sims].index(doc_id)
+    ranks.append(rank)
+    second_ranks.append(sims[1])
+
+import collections
+counter = collections.Counter(ranks)
+print(counter)
+
+import random
+doc_id = random.randint(0, len(train_corpus) - 1)
+
+# Pick a random document from the test corpus and infer a vector from the model
+doc_id = random.randint(0, len(train_corpus) - 1)
+doc_id = 188
+
+inferred_vector = model.infer_vector(train_corpus[doc_id][0])
+sims = model.docvecs.most_similar([inferred_vector], topn=len(model.docvecs))
+
+# df01.iloc[doc_id].text[:1000]
+' '.join(train_corpus[doc_id].words)[:1000]
+
+
+# Find top3 most similar
+print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
+for label, index in [('TOP', 1), ('SECOND', 2), ('THIRD', 3)]:
+    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)[:1000]))
+
+
+# Compare and print the most/median/least similar documents from the train corpus
+# print('Test Document ({}): «{}»\n'.format(doc_id, ' '.join(train_corpus[doc_id])))
+print(u'SIMILAR/DISSIMILAR DOCS PER MODEL %s:\n' % model)
+for label, index in [('MOST', 1), ('MEDIAN', len(sims)//2), ('LEAST', len(sims) - 1)]:
+    print(u'%s %s: «%s»\n' % (label, sims[index], ' '.join(train_corpus[sims[index][0]].words)[:1000]))
+
 
 
 # ============================================================================================
